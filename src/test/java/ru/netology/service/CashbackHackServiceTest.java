cb6d87426423fb32f49dc082ebac084a8ed9472d
Evgeniya-EVA <evgeniya.anisimova.eva@gmail.com>
2019-12-16 17:19:33 +0300

    feat: more Test for 100% Code Coverage

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,8 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
+    void shouldShowExceptionIfAmountIsUnderZero() {
+        // подготовка
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = -100;
+        assertThrows(IllegalArgumentException.class, () -> cashbackHackService.remain(amount));
+    }
+
+    @Test
     void return100IfAmountIs900() {
-        // подготовка данных
+        // подготовка
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
 
@@ -22,9 +31,23 @@ class CashbackHackServiceTest {
 
     @Test
     void returnZeroIfAmountIs1000() {
-        // подготовка данных
+        // подготовка
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
+
+        // выполнение действия
+        int actual = cashbackHackService.remain(amount);
+
+        // сравнение результата
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void returnZeroIfAmountIs1100() {
+        // подготовка
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1100;
 
         // выполнение действия
         int actual = cashbackHackService.remain(amount);
