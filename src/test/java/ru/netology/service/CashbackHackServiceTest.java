bf531feedba3c732cd292150eccae1104a446953
Evgeniya-EVA <evgeniya.anisimova.eva@gmail.com>
2019-12-17 09:46:55 +0300

    feat: testdata.csv is added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,59 +1,32 @@
 package ru.netology.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
     @Test
-    void shouldShowExceptionIfAmountIsUnderZero() {
+    void shouldShowExceptionIfAmountIsOrUnderZero() {
         // подготовка
         CashbackHackService cashbackHackService = new CashbackHackService();
 
-        int amount = -100;
+        int amount = 0;
         assertThrows(IllegalArgumentException.class, () -> cashbackHackService.remain(amount));
     }
 
-    @Test
-    void return100IfAmountIs900() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testdata.csv", numLinesToSkip = 1, encoding = "UTF-8")
+    void remainCalculation(int amount, int expected, String message) {
         // подготовка
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 900;
 
         // выполнение действия
         int actual = cashbackHackService.remain(amount);
 
         // сравнение результата
-        int expected = 100;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void returnZeroIfAmountIs1000() {
-        // подготовка
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
-
-        // выполнение действия
-        int actual = cashbackHackService.remain(amount);
-
-        // сравнение результата
-        int expected = 0;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void returnZeroIfAmountIs1100() {
-        // подготовка
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1100;
-
-        // выполнение действия
-        int actual = cashbackHackService.remain(amount);
-
-        // сравнение результата
-        int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, message);
     }
 }
